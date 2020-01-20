fba7b27f99bbf686377c128b7b7d8127436a90ef
Ashini95 <ashiddk95@gmail.com>
2020-01-20 20:05:21 +0000

    Add new features to diary application

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ApplicationRecord
+  validates :description, presence: true
+  validates :date,presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+  get 'entries/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :entries
 
-  root 'welcome#index'
+  root 'entries#index'
+  get 'entries/new' => 'entries#new'
 end
